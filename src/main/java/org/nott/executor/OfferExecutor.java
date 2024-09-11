9ff7d160a14928e9d717d213ca1bafdf2b80c3ed
nott <isnott1028@outlook.com>
2024-09-11 14:16:07 +0800

    add: Offer Command.

---
+++ src/main/java/org/nott/executor/OfferExecutor.java
@@ -0,0 +1,20 @@
+package org.nott.executor;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * 悬赏命令-执行器
+ * @author Nott
+ * @date 2024-9-11
+ */
+public class OfferExecutor implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return false;
+    }
+}
