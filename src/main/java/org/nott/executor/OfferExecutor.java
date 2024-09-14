d45000690ab4f67161b6cb6ad61e21a4e32a616d
Nott <nott@163.com>
2024-09-14 11:34:46 +0800

    update: message.yml

--- src/main/java/org/nott/executor/OfferExecutor.java
+++ src/main/java/org/nott/executor/OfferExecutor.java
@@ -1,8 +1,10 @@
 package org.nott.executor;
 
+import lombok.Data;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -10,8 +12,14 @@ import org.jetbrains.annotations.NotNull;
  * @author Nott
  * @date 2024-9-11
  */
+@Data
 public class OfferExecutor implements CommandExecutor {
 
+    private Plugin plugin;
+
+    public OfferExecutor(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
